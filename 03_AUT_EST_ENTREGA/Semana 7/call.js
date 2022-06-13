82b2babbb7936e0535d3f5e49be18380ebbb90ee
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2022-06-12 21:41:57 -0300

    Currículo semana 7

---
+++ 03_AUT_EST_ENTREGA/Semana 7/call.js
@@ -0,0 +1,88 @@
+const express = require ("express");
+
+const hostname = "127.0.0.1";
+const port = 3100;  
+
+const app = express(); 
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = './backend/curriculosemana6.db';
+
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+
+
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM Habilidades ORDER BY Num ASC';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+app.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO Habilidades (Num, Nome) VALUES ('" + req.body.title + "', estudar 12 horas seguidas)";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE Habilidades SET title = '" + req.body.title + "' WHERE Num = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM Habilidades WHERE Num = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+
+app.get("/", (req, res) => { 
+    res.sendFile(__dirname + "/public/curriculosemana6.html");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
