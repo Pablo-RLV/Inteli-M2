1162c3fff1cc46bdb4a18239b2fc142bfa656d73
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2022-06-19 19:28:38 -0300

    Exercício 5 adicionado

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Semana 7/Exercício 5.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/Semana 7/Exercício 5.js
@@ -1,54 +1,18 @@
-var needCreateForm = 1;
-$('#createForm').on('click', () =>{
-    var averageStudent = '';
-    var wholeAverage = 0; 
-    var examAverage = 0;
-    var jobAverage = 0;
-    var arrayNumbersExam = [];
-    var arrayNumbersJob = [];
+function calcular(){
+    var n1 = document.getElementById("num1").value
+    var n2 = document.getElementById("num2").value
+    var lista_primos = []
 
-    if(needCreateForm == 1){
-        var qtdStudents = $('#students').val();
-        var output = '<br>';
-
-        for(var i = 0; i < qtdStudents; i++){
-            output += `Aluno ${i + 1} - Nota Prova: <input type="number" class="exam"> Nota trabalho: <input type="number" class="job"><br><br>`;
+    for(var i = n1; i <= n2; i++){
+        var num_divisores = 0
+        for(var possiveis_divisores = 0; possiveis_divisores <= n2; possiveis_divisores++){
+            if(i % possiveis_divisores == 0){
+                num_divisores += 1
+            }
         }
-        output += '<input type="submit" value="Calcular" id="doCalc">'; 
-        $('#form').append(`${output}`);
-
-        var notesExam = document.querySelectorAll('input.exam');
-        var notesJob = document.querySelectorAll('input.job');
+        if(num_divisores == 2){
+            lista_primos.push(i)
+        }
     }
-
-    $('#doCalc').on('click', () =>{
-        for(var i = 0; i < qtdStudents; i++){
-            averageStudent += `<br> Média do estudante ${i + 1}: ${(parseInt(notesExam[i].value) + parseInt(notesJob[i].value)) / 2}<br>`;
-            wholeAverage += parseInt(notesExam[i].value) + parseInt(notesJob[i].value);
-            examAverage += parseInt(notesExam[i].value);
-            jobAverage += parseInt(notesJob[i].value);
-            arrayNumbersExam.push(notesExam[i].value);
-            arrayNumbersJob.push(notesJob[i].value);
-        }
-        $('#form').append(`<hr> ${averageStudent}`);
-        wholeAverage = wholeAverage / qtdStudents;
-        $('#form').append(`<br><br> Média geral: ${wholeAverage}<br>`);
-        examAverage = examAverage / qtdStudents;
-        $('#form').append(`<br><br> Média aritimética prova: ${examAverage}<br>`);
-        jobAverage = jobAverage / qtdStudents;
-        $('#form').append(`<br><br> Média aritimética trabalho: ${jobAverage}<br>`)
-        arrayNumbersExam = arrayNumbersExam.sort((a, b) => a - b);
-        $('#form').append(`<br><br> Menor nota prova: ${arrayNumbersExam[0]} <br><br> Maior nota prova: ${arrayNumbersExam[arrayNumbersExam.length - 1]}<br>`);
-        arrayNumbersJob = arrayNumbersJob.sort((a, b) => a - b);
-        $('#form').append(`<br> Menor nota trabalho: ${arrayNumbersJob[0]} <br><br> Maior nota trabalho: ${arrayNumbersJob[arrayNumbersJob.length - 1]}<br>`);
-    });
-
-    needCreateForm = 0;
-});
-
-
-
-
-///////////////////////////////////////////////
-////////////////////////////
-/////////////////////////
+    document.getElementById("resultado").innerHTML = lista_primos
+}
